5ea17476e7f33b6c7bbdfaf1e2a64e75758e9363
tbm5k <tedburg5@gmail.com>
2023-05-02 23:57:11 +0300

    📄 file reader

--- reader.go
+++ reader.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+)
+
 
 func main() {
 	fmt.Println("Weuh")
+
+	f, err := os.Open("sample.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
