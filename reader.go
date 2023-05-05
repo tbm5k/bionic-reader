a672c1b67b8b8fe688a510ba17163c6553ef412d
tbm5k <tedburg5@gmail.com>
2023-05-05 23:53:17 +0300

    ❌ dslispak does not solve our problem

--- reader.go
+++ reader.go
@@ -1,27 +1,49 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+
 	"github.com/dslipak/pdf"
+	"github.com/dslipak/pdf/core"
 )
 
 
 func main() {
-	r, err := pdf.Open("sample.pdf")
-
+	_, err := readWithStles("sample.pdf")
 	if err != nil {
 		panic(err)
 	}
 
-	b, err := r.GetPlainText()
+}
+
+func readWithStles (path string) (string, error){
+	r, err := pdf.Open(path)
 
 	if err != nil {
-		panic(err)
+		return "", nil
 	}
 
-	var buf bytes.Buffer
-	buf.ReadFrom(b)
+	for page := 1; page <= r.NumPage(); page++ {
+		p := r.Page(page)
 
-	fmt.Println(buf.String())
+		if p.V.IsNull() {
+			continue
+		}
+
+		// var lastTextStyle pdf.Text
+
+		// texts := p.Content().Text
+
+		fmt.Println()
+		// for _, text := range texts {
+		// 	if text == lastTextStyle {
+		// 		lastTextStyle.S = lastTextStyle.S + text.S
+		// 	} else {
+		// 		fmt.Printf("font: %s, font size: %f, x: %f, y: %f, content: %s \n", lastTextStyle.Font, lastTextStyle.FontSize, lastTextStyle.X, lastTextStyle.Y, lastTextStyle.S)
+		// 		lastTextStyle = text
+		// 	}
+		// }
+	}
+
+	return "", nil
 }
