250166cd1f63a0c18f13ae94a269d55d963090d7
tbm5k <tedburg5@gmail.com>
2023-08-12 18:13:33 +0300

    🐾 a step

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ## 📖 binary reader
-Binary reader have a study showing that for a piece of literature, if you make the first half of every word bold 
-this dramatically increases your reading speeds. 
 
-So this is a Go implementation of that. 
+Binary reader have a study showing that for a piece of literature, if you make the first half of every word bold
+this dramatically increases your reading speeds.
+
+So this is a Go implementation of that.
+
+### installation
+
+run
+`go install code.sajari.com/docconv/docd@latest`
--- reader.go
+++ reader.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/dslipak/pdf"
+	"code.sajari.com/docconv"
 )
 
 
 func main() {
-	r, err := pdf.Open("sample.pdf")
 
-	if err != nil {
-		panic(err)
+    res, err := docconv.ConvertPath("samplepdf.pdf")
+    if err != nil {
+		log.Fatal(err)
 	}
-
-	pages := r.NumPage()
-
-
-	for pageNumber := 1; pageNumber <= pages; pageNumber++ {
-		p := r.Page(pageNumber)
-
-		if p.V.IsNull() {
-			continue
-		}
-
-		texts := p.Content().Text
-		for _, text := range texts {
-			fmt.Printf("content: %s \n", text.S)
-		}
-
-	}
+    
+    text := res.Body
+    fmt.Println(text)
 }
